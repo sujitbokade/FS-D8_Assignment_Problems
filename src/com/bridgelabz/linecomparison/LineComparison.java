5f9159b5a03748e11b042b3a871b7b761a8ac5ba
sujit <sujitbokade2014@gmail.com>
2022-07-16 15:56:52 +0530

    Check Equality

--- src/com/bridgelabz/linecomparison/LineComparison.java
+++ src/com/bridgelabz/linecomparison/LineComparison.java
@@ -6,6 +6,7 @@ public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Program!");
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Line Points :");
         System.out.println("Enter a Point of x1 ");
         int x1 = sc.nextInt();
         System.out.println("Enter a Point of y1");
@@ -15,7 +16,29 @@ public class LineComparison {
         System.out.println("Enter a Point of y2");
         int y2= sc.nextInt();
 
-        Double Length=Math.sqrt(Math.pow((x2-x1),2))+(Math.pow((y2-y1),2));
-        System.out.println("Length of Line is "+Length);
+        Double length1=Math.sqrt(Math.pow((x2-x1),2))+(Math.pow((y2-y1),2));
+        System.out.println("Length of Line is "+length1);
+        System.out.println();
+
+        System.out.println("Enter Second Line Points :");
+        System.out.println("Enter a Point of x3 ");
+        int x3 = sc.nextInt();
+        System.out.println("Enter a Point of y3");
+        int y3=sc.nextInt();
+        System.out.println("Enter a Point of x4");
+        int x4= sc.nextInt();
+        System.out.println("Enter a Point of y4");
+        int y4= sc.nextInt();
+
+        Double length2=Math.sqrt(Math.pow((x4-x3),2))+(Math.pow((y4-y3),2));
+        System.out.println("Length of Line is "+length2);
+
+        boolean result=length1.equals(length2);
+        if(result==true){
+            System.out.println("Both Lines are Equal!");
+        }
+        else {
+            System.out.println("Lines are Not Equal!");
+        }
     }
 }
