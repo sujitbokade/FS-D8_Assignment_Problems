6f8ca8de0041588956263e088c4f5ab0412ac06e
sujit <sujitbokade2014@gmail.com>
2022-07-16 15:47:19 +0530

    Calculte Length of Line

--- src/com/bridgelabz/linecomparison/LineComparison.java
+++ src/com/bridgelabz/linecomparison/LineComparison.java
@@ -1,7 +1,21 @@
 package com.bridgelabz.linecomparison;
 
+import java.util.Scanner;
+
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Program!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Point of x1 ");
+        int x1 = sc.nextInt();
+        System.out.println("Enter a Point of y1");
+        int y1=sc.nextInt();
+        System.out.println("Enter a Point of x2");
+        int x2= sc.nextInt();
+        System.out.println("Enter a Point of y2");
+        int y2= sc.nextInt();
+
+        Double Length=Math.sqrt(Math.pow((x2-x1),2))+(Math.pow((y2-y1),2));
+        System.out.println("Length of Line is "+Length);
     }
 }
