b6ddd07651a1b77a5f61d8ff669f0232de564526
sujit <sujitbokade2014@gmail.com>
2022-07-16 17:32:30 +0530

    Added Part Time Hrs.

--- src/com/bidgelabz/employeewage/EmployeeWage.java
+++ src/com/bidgelabz/employeewage/EmployeeWage.java
@@ -1,17 +1,21 @@
 package com.bidgelabz.employeewage;
 
 public class EmployeeWage {
-    static final int Employee_Present=1;
-    static final int Wage_per_Hr=20;
-    static final int Full_Day_Hr=10;
+    static final int IS_FULL_TIME=1;
+    static final int IS_PART_TIME=2;
+    static final int WAGE_PER_HR=20;
+    static final int FULL_DAY_HR=10;
+    static final int PART_TIME_HR=5;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage !");
-        double empCheck=Math.floor(Math.random()*10) % 2;
-
+        double empCheck=Math.floor(Math.random()*10) % 3;
         int dailywage=0;
-        if(empCheck==Employee_Present) {
-            System.out.println("employee is present");
-            dailywage=Wage_per_Hr*Full_Day_Hr;
+        if(empCheck==IS_FULL_TIME) {
+            System.out.println("employee is full time");
+            dailywage=WAGE_PER_HR * FULL_DAY_HR;
+        } else if (empCheck==IS_PART_TIME) {
+            System.out.println("employee is part time");
+            dailywage=WAGE_PER_HR * PART_TIME_HR;
         }
         else {
             System.out.println("employee is absent");
