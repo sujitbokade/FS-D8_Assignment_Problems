cabbaa0b62a77c8b211406c413cdef19fc7180b5
sujit <sujitbokade2014@gmail.com>
2022-07-16 17:23:48 +0530

    Employee Attendence

--- src/com/bidgelabz/employeewage/EmployeeWage.java
+++ src/com/bidgelabz/employeewage/EmployeeWage.java
@@ -1,7 +1,14 @@
 package com.bidgelabz.employeewage;
 
 public class EmployeeWage {
+    static final int Employee_Present=1;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Program!");
+        double empCheck=Math.floor(Math.random()*10) % 2;
+        if(empCheck==Employee_Present)
+            System.out.println("employee is present");
+        else
+            System.out.println("employee is absent");
+
     }
 }
