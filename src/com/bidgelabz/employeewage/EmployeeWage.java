6cd18b6f94e238f74ac62cb3e2088665cfa09b34
sujit <sujitbokade2014@gmail.com>
2022-07-18 21:35:53 +0530

    Calculate Employee Wages for Multiple Companies

--- src/com/bidgelabz/employeewage/EmployeeWage.java
+++ src/com/bidgelabz/employeewage/EmployeeWage.java
@@ -1,49 +1,47 @@
 package com.bidgelabz.employeewage;
 
 public class EmployeeWage {
-        static final int IS_FULL_TIME = 1;
-        static final int IS_PART_TIME = 2;
-        static final int WAGE_PER_HOUR = 20;
-        static final int WORKING_DAY_PER_MONTH = 20;
-        static final int TOTAL_WORK_HOURS = 100;
+    static final int IS_FULL_TIME = 1;
+    static final int IS_PART_TIME = 2;
 
+    static void calculateWage(String company, int wagePerHr,int daysPerMonth,int workHrPerMonth){
 
-        static void calculateWage(){
+        int empHrs;
+        int totalHours=0;
+        int dailyWage;
+        int day=0;
+        int totalWage=0;
 
-            int empHr;
-            int totalHour=0;
-            int dailyWage;
-            int day=0;
-            int totalWage=0;
+        while((totalHours<workHrPerMonth) && (day<daysPerMonth)) {
 
-            while((totalHour<TOTAL_WORK_HOURS) && (day<WORKING_DAY_PER_MONTH)) {
-
-                int attendance = (int)(Math.floor(Math.random()*10))%3;
-                switch (attendance) {
-                    case IS_FULL_TIME:
-                        empHr=10;
-                        break;
-                    case IS_PART_TIME:
-                        empHr=5;
-                        break;
-                    default:
-                        empHr=0;
-                        break;
-                }
-                totalHour+=empHr;
-                dailyWage = WAGE_PER_HOUR*empHr;
-                totalWage+=dailyWage;
-                day++;
-                System.out.println("Day "+day+" Emp work hour "+empHr+ " wage is "+dailyWage);
+            int attendance = (int)(Math.floor(Math.random()*10))%3;
+            switch (attendance) {
+                case IS_FULL_TIME:
+                    empHrs=8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs=4;
+                    break;
+                default:
+                    empHrs=0;
+                    break;
             }
-            System.out.println("Total work hours is : "+totalHour);
-            System.out.println("Total work days are: " +day);
-            System.out.println("Total wage is: "+totalWage);
-
+            totalHours+=empHrs;
+            dailyWage = wagePerHr*empHrs;
+            totalWage+=dailyWage;
+            day++;
+            System.out.println("Day "+day+" Emp work hour "+empHrs+ " wage is "+dailyWage);
         }
-
-        public static void main(String[] args) {
-            calculateWage();
-        }
+        System.out.println("Total work hours is : "+totalHours);
+        System.out.println("Total work days are: " +day);
+        System.out.println("Total Wage of company "+company+ " is "+ totalWage);
 
     }
+
+    public static void main(String[] args) {
+        calculateWage("PhrandeSpaces",15,20,100);
+        System.out.println();
+        calculateWage("Hassan Technology",20,22,100);
+    }
+
+}
